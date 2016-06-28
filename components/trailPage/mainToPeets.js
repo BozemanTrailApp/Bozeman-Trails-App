333c92cac882e0c764d67bb539469dd3b1adfd2d
Jeslyn Tweedie <jeslyntweedie@live.com>
2016-06-28 10:06:16 -0600

    maps

--- components/trailPage/mainToPeets.js
+++ components/trailPage/mainToPeets.js
@@ -1,11 +1,14 @@
 var React = require('react');
 
 var mainToPeets = React.createClass({
+
+	url: "http://snippets.mapmycdn.com/routes/view/embedded/1129495984?width=600&height=400&elevation=true&line_color=E60f0bdb&rgbhex=DB0B0E&distance_markers=0&unit_type=imperial&map_mode=HYBRID&last_updated=2016-06-24T11:15:25-06:00",
+
 	render: function(){
 		return (
 			<div>
 				<h1>Main Street to Peet's Hill</h1>
-				
+				<iframe height="600px" width="600px" src= {this.url}/>
 			</div>
 			)
 	}
--- components/trailPage/sourdough.js
+++ components/trailPage/sourdough.js
@@ -2,10 +2,16 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var Sourdough = React.createClass({
+
+	url: "http://snippets.mapmycdn.com/routes/view/embedded/1126105552?width=600&height=400&elevation=true&line_color=E60f0bdb&rgbhex=DB0B0E&distance_markers=0&unit_type=imperial&map_mode=HYBRID&last_updated=2016-06-22T12:16:51-06:00",
+
 	render: function(){
 		return (
 			<div>
 				<h1>Sourdough Trail</h1>
+					<div className ="sourMap">
+						<iframe height="600px" width="600px" src= {this.ur}/>
+					</div>
 				
 			</div>
 			)
