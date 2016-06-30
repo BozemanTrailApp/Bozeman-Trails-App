b925eaa748e33d5648a7e80abe0a96c6db81ff44
Lance Barney <lbarney@littleappletech.com>
2016-06-30 16:29:55 -0600

    update

--- components/trailCard.js
+++ components/trailCard.js
@@ -13,18 +13,18 @@ var TrailCard = React.createClass({
 	render: function(){
 			return(
 				<div>
-					
-				<div className="trailCard">
-					<h3>{this.props.oneTrail.trailName}</h3>
-					<h3>Difficulty: {this.props.oneTrail.difficulty} </h3>
-					<h3>Length: {this.props.oneTrail.length} </h3>
-					<h3>Location: {this.props.oneTrail.location} </h3>
-					<h3> {this.props.oneTrail.description} </h3>
-					<h3> {this.props.oneTrail.directions}</h3>
-					<h3> {this.props.oneTrail.trailHeadUrl} </h3>
-					<h3> {this.props.oneTrail.comments} </h3>
-					<h3> {this.props.oneTrail.userImage}</h3>
+					<div>
+						<h3>{this.props.oneTrail.trailName}</h3>
+						<h3>Difficulty: {this.props.oneTrail.difficulty} </h3>
+						<h3>Length: {this.props.oneTrail.length} </h3>
+						<h3>Location: {this.props.oneTrail.location} </h3>
+						<h3> {this.props.oneTrail.description} </h3>
+						<h3> {this.props.oneTrail.directions}</h3>
+						<h3> {this.props.oneTrail.trailHeadUrl} </h3>
+						<h3> {this.props.oneTrail.comments} </h3>
+						<h3> {this.props.oneTrail.userImage}</h3>
 				</div>
+				
 				<div>
 					<iframe height = '600px' width ='600px' src = {this.props.oneTrail.mapUrl}/>
 				</div>
