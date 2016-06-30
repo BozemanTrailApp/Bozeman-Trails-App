2ed7e557a47a2a1ede30d317dc0a49840158c2ce
Lance Barney <lbarney@littleappletech.com>
2016-06-30 15:57:58 -0600

    fixing routes

--- components/config/routes.js
+++ components/config/routes.js
@@ -9,23 +9,16 @@ var Router = ReactRouter.Router;
 var IndexRoute = ReactRouter.IndexRoute;
 var hashHistory = ReactRouter.hashHistory;
 
-// import {Router, Route, hashHistory, IndexRoute } from 'react-router'; 
-// this is a Method on React router "Above"
+
 
 
 
 var Main = require('./../main');
-var Profile = require('./../profile');
-// var TrailsApp = require('./../trailsApp');
+var Profile = require('./../profile'); 
+var TrailsApp = require('./../trailsApp');
 var Home = require('./../home');
 var Login = require('./../login');
-// var UserLog = require('./../userLog');
-// var Sourdough = require('./../trailPage/sourdough');
-// var Leverich = require('./../trailPage/leverich');
-// var MainToPeets = require('./../trailPage/mainToPeets');
-// var Gallagator = require('./../trailPage/gallagator');
-// var Sypes = require('./../trailPage/sypes');
-// var THEm = require('./../trailPage/theM');
+
 
 
 ReactDom.render(
@@ -33,12 +26,10 @@ ReactDom.render(
 		
 		<Route path='/' component = {Main}>
 			<IndexRoute component = {Home}/>
-			<Route path ='/profile' component = {Profile}/>
-			<Route path = '/home' component = {Home}/>
-	
-
-			
-			<Route path = '/login' component = {Login}/>
+				<Route path ='/profile' component = {Profile}/>
+				<Route path = '/home' component = {Home}/>
+				<Route path = '/trails' component = {TrailsApp}/>
+				<Route path = '/login' component = {Login}/>
 
 			
 		</Route>
