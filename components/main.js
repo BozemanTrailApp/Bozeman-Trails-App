60e92bf31476d812657bc54739ed52d28e017ae2
Morsdeimos <longden.jonathanedgar@gmail.com>
2016-07-01 14:04:10 -0600

    log out button across the boards

--- components/main.js
+++ components/main.js
@@ -2,38 +2,100 @@ var React = require('react');
 var Link = require('react-router').Link;
 
 var Main = React.createClass({
+	getInitialState: function(){
+		return {
+			user: ""
+		}
+	},
+
+	getOneUserFromServer: function(){
+		var self = this;
+			$.ajax({
+				method:'GET',
+				url:'/user'
+			}).done(function(data){
+				console.log(data);
+				self.setState({ user: data });
+			})
+	},
+
+	componentDidMount: function(){
+		this.getOneUserFromServer();
+	},
+	logoutUser: function(user){
+		var self = this;
+		//var user = self.state.user;
+		$.ajax({
+			url: '/logout',
+			method: 'GET', 
+			success: function(data){
+				$.ajax({
+				method:'GET',
+				url:'/user'
+			}).done(function(data){
+				console.log(data);
+				self.setState({ user: data });
+				window.location = "http://localhost:8000"
+				})
+				//self.setState( data );
+				//console.log("Logout successful.", data);
+			}.bind(self),
+			error: function(xhr, status, err){
+				console.error('/logout', status, err.toString());
+			}
+		})
+	},
 	render: function(){
-		return (
+		
+		if(this.state.user.user !== "anonymous"){
+			return(
 			<div>
 				<div>
-					<h1 className="theTitle"> Bozeman.by.trail </h1>
-				</div>
-
-					<div className= "fixedbuttons">
-					<Link to = '/home'>
-						<button className = 'homemain btn btn-success btn-md'>H O M E</button>
-					</Link>
-					<Link to = '/profile'>
-						<button className = 'profilemain btn btn-default btn-md'>P R O F I L E</button>
-					</Link>
-					<Link to ='/login'>
-						<button className = 'loginmain btn btn-default btn-md'> login </button>
-						</Link>
+				<div>
+						<h1 className="theTitle"> Bozeman.by.trail </h1>
 					</div>
-
-						
-
-					{this.props.children}
+						<div className= "fixedbuttons">
+							<Link to = '/home'>
+								<button className = 'homemain btn btn-success btn-md'>H O M E</button>
+							</Link>
+							<Link to = '/profile'>
+								<button className = 'profilemain btn btn-default btn-md'>P R O F I L E</button>
+							</Link>
+							<Link to = '/signout'>
+								<button className = 'signoutmain btn btn-default btn-md' onClick={ this.logoutUser } 
+								type="submit" name="action">Sign out</button>
+							</Link>
+						</div>	
+						{this.props.children}	
+						<footer id = 'footer'>
+							<p>Brought to you by the minds of Jonathan, Jeslyn, and Lance</p>
+						</footer>
 					
-					
-						
+				</div>
+			</div>
+					)
+		} else {
+
+			return(
+			<div>
+					<div>
+						<h1 className="theTitle"> Bozeman.by.trail </h1>
+					</div>
+						<div className= "fixedbuttons">
+						<Link to = '/home'>
+							<button className = 'homemain btn btn-success btn-md'>H O M E</button>
+						</Link>
+						<Link to ='/login'>
+							<button className = 'loginmain btn btn-default btn-md'> login </button>
+						</Link>
+						</div>
+						{this.props.children}	
 				<footer id = 'footer'>
 					<p>Brought to you by the minds of Jonathan, Jeslyn, and Lance</p>
 				</footer>
-				
-
 			</div>
-			)
+					)
+		}
 	}
 });
 
