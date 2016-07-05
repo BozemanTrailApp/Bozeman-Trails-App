afb55b5cf45b1cff7e2239880399636eddcc32b6
Morsdeimos <longden.jonathanedgar@gmail.com>
2016-07-05 10:34:40 -0600

    functioning profile on Postman

--- components/profile.js
+++ components/profile.js
@@ -2,8 +2,8 @@ var React = require('react');
 var Link = require('react-router').Link;
 
 
-var UserLog = require('./userLog.js');
-var UserLogData = require('./userLogData.js');
+var UserLog = require('./login/userLog.js');
+var UserLogData = require('./login/userLogData.js');
 
 
 
@@ -23,12 +23,28 @@ var Profile = React.createClass({
 				self.setState({ user: data });
 			})
 	},
+	addHikeToUser: function(){
+		$.ajax({
+			method: 'PUT',
+			url: '/user/:id',
+			data: user,
+			success: function(data){
+				console.log("Adding Miles", data);
+				self.setState({ user : data });
+				alert("Success on Logging your Miles Hiked!");
+			},
+			error: function(xhr, status, err){
+				console.error('Failed to Add Miles', status, err.toString())
+				alert('Failed on Logging your Miles!');
+			}
+		})
+	},
 	componentDidMount: function(){
 		this.getOneUserFromServer();
 	},
 	render: function(){
-			if(this.state.user.user !== "anonymous"){
-			return(
+			
+		return (	
 		<div>
 			<div className = "profilecontainer">
 			   	<div className = "profile">
@@ -45,30 +61,14 @@ var Profile = React.createClass({
 					<h3> Gender: {this.state.user.gender} </h3>
 
 
-					
+					<UserLogData />
+
 					</div>
-				 </div>
+				</div>
 			</div>
 		</div>
 			
-			
-
 			)
-
-		} else {
-
-		return(
-			<div>
-				<div className = "profilecontainer">
-			   		<div className = "profile">
-					<UserLogData />
-			   		 <h1>Please sign-in to view profile</h1>
-
-				     </div>
-				</div>
-			</div>
-			)
-		}
 
 	}
 
