1e831893c44a99e7fcf6114177f0f750b6f57dad
Jeslyn Tweedie <jeslyntweedie@live.com>
2016-07-05 10:36:04 -0600

    commit

--- models/profileSchema.js
+++ models/profileSchema.js
@@ -4,6 +4,6 @@ var profileSchema = new mongoose.Schema({
 	hikeName: {type: String},
 	date: {type: Number},
 	miles: {type: String},
-	user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+
 });
 module.exports = profileSchema;
