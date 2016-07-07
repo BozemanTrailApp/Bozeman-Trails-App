ed51b6b1c8c8b475ef80eccb0e7620627aca7473
Morsdeimos <longden.jonathanedgar@gmail.com>
2016-07-07 12:46:18 -0600

    saved work

--- components/login/userLog.js
+++ components/login/userLog.js
@@ -1,26 +1,31 @@
 const React = require('react');
 const Link = require('react-router').Link;
+const ReactDOM = require('react-dom');
 
+var UserLog = React.createClass({
+	componentDidMount: function (){
+  		var element = ReactDOM.findDOMNode(this.refs.dropdown)
+  			$(element).ready(function() {
+    		$('select').material_select();
+ 			 });
+  			//$(this.refs.yourSelectTag).material_select(this._onHikeNameChange.bind(this));
+	},
+	render: function (){
 
-class UserLog extends React.Component{
-	render(){
 		return(
 			
-
-			<div>
-			<div className = "wholeform">
+				<div>
+				<div className = "wholeform">
 		    	<form className="col s6 hikeform" onSubmit={ this.props.handleHikeLogSubmit }>
 		      	<div className="row">
-		        	<div className="input-field col s4">
-		         		<input  type="text" onChange={ this.props.onHikeNameChange } value={ this.props.hikeName } className="validate"/>
-		          		<label>Hike</label>
-		        	</div>
+        			<div className="input-field col s4">
+	 		         	<input  type="text" onChange={ this.props.onHikeNameChange } value={ this.props.hikeName } className="validate"/>
+ 		          		<label>Hike</label>
+ 		        	</div>
 				</div>
 		      	<div className="row">
 		        	<div className="input-field col s4">
-
 		         		<input  type="date" onChange={ this.props.onDateChange } value={ this.props.date} className="validate"/>
-
 		        	</div>
 				</div>
 		      	<div className="row">
@@ -34,14 +39,58 @@ class UserLog extends React.Component{
 	  		</form>
 	 	</div>
 	 </div>
-	
-
-
-
-
 			)
 	}
-};
-
+});
 
 module.exports = UserLog;
+
+
+
+
+
+
+// <div className="input-field col s4">
+// 		      <select ref="dropdown" defaultValue="1">
+// 		        <option value="" disabled>Choose a Trail!</option>
+// 		        <option value="1">Sourdough Trail</option>
+// 		        <option value="2">Leverich Canyon Mountain Trail</option>
+// 		        <option value="3">Main Street to Peet's Hill</option>
+// 		        <option value="4">Gallagator Linear Trail</option>
+// 		        <option value="5">Sypes Canyon</option>
+// 		      </select>
+// 		      <label>Materialize Select</label>       
+// 		</div>
+
+//<select ref="dropdown"  defaultValue="1" onChange={ this.props.onHikeNameChange } value={this.props.hikeName}>
+//<option disabled>Choose a Trail!</option>
+//<option value="SourdougTrail" placeholder="Sourdough Trail">Sourdough Trail</option>
+//<option value="Leverich" placeholder="Leverich Canyon Mountain Trail">Leverich Canyon Mountain Trail</option>
+//<option value="" placeholder="Main Street to Peet's Hill">Main Street to Peet's Hill</option>
+//<option value="" placeholder="Gallagator Linear Trail">Gallagator Linear Trail</option>
+//<option value="" placeholder="Sypes Canyon">Sypes Canyon</option>
+//</select>   
+
+
+// <div className="row">
+// 	<div className="input-field col s4">
+// 	<select onChange={ this.props.onHikeNameChange } value={this.props.hikeName} >
+//  	{createItem }
+// 	</select>	   
+// 	</div>
+// </div>
+
+// var createItem = this.props.hikeName.map(function (item, key) {
+// 			console.log(item.name);
+//       						return (
+
+// 		      						<option 
+// 		      							key={key} 
+// 		      							value={item.value}>
+// 		      							{item.name}
+// 		      						</option>
+
+// 	    					)
+//     					});
+
+// 			console.log(createItem);
