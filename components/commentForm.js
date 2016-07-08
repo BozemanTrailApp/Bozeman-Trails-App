5450a15f44b9d90950273c1147be06c9f64edfa6
Lance Barney <lbarney@littleappletech.com>
2016-07-08 10:32:18 -0600

    added a paper.jpg

--- components/commentForm.js
+++ components/commentForm.js
@@ -13,7 +13,26 @@ var CommentForm = React.createClass({
 			body: null
 		}
 	},
-	addCommentToTrail: function(){
-		
+	handleBodyChange: function(event){
+		this.setState({body: event.target.value})
+	},
+	handleCommentSubmit: function(event){
+		event.preventDefault();
+		var comment = {};
+		comment.user = this.state.user;
+		comment.date = this.state.date;
+		comment.body = this.state.body;
+		this.handleNewComment(comment);
+		this.setState({user: '', date: '', body: ''});
+	},
+	handleNewComment: function(comment){
+		var self = this;
+		$.ajax({
+			url: '/trails',
+			method: 'PUT',
+			dataType: 'json',
+			data: comment,
+			
+		})
 	}
 });
